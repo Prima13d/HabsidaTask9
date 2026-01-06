4ac8ef268accdaa6cd14e133b918a8444c8799ac
prima <be-alexey@mail.ru>
2026-01-06 23:28:02 +0900

    finised task 11

--- src/main/java/habsida/spring/boot_security/demo/controller/AdminPageController.java
+++ src/main/java/habsida/spring/boot_security/demo/controller/AdminPageController.java
@@ -8,58 +8,12 @@ import habsida.spring.boot_security.demo.model.User;
 import habsida.spring.boot_security.demo.service.UserService;
 
 @Controller
-@RequestMapping("/admin/users")
+@RequestMapping("/admin")
 public class AdminPageController {
 
-    private final UserService userService;
-
-    @Autowired
-    public AdminPageController(UserService userService) {
-        this.userService = userService;
-    }
-
-    @GetMapping
-    public String showUsers(Model model) {
-        model.addAttribute("users", userService.getAllUsers());
+    @GetMapping("/users")
+    public String showAdminPage() {
         return "users";
     }
-
-    @GetMapping("/add")
-    public String showAddUserForm() {
-        return "add_user";
-    }
-
-    @PostMapping("/add")
-    public String addUser(@RequestParam("userFirstName") String userFirstName,
-                          @RequestParam("userFamilyName") String userFamilyName,
-                          @RequestParam("username") String username,
-                          @RequestParam("password") String password,
-                          @RequestParam("role") String role) {
-        userService.saveUser(userFirstName, userFamilyName, username, password, role);
-        return "redirect:/admin/users";
-    }
-
-    @GetMapping("/delete/{id}")
-    public String deleteUser(@PathVariable("id") Long id) {
-        userService.removeUserById (id);
-        return "redirect:/admin/users";
-    }
-
-    @GetMapping("/edit/{id}")
-    public String showEditForm(@PathVariable("id") Long id, Model model) {
-        User user = userService.getUserById(id);
-        model.addAttribute("user", user);
-        return "edit_user";
-    }
-
-    @PostMapping("/edit")
-    public String updateUser(@RequestParam("id") Long id,
-                             @RequestParam String userFirstName,
-                             @RequestParam String userFamilyName) {
-        userService.updateUser(id, userFirstName, userFamilyName);
-        return "redirect:/admin/users";
-    }
-
-
 
 }
--- src/main/java/habsida/spring/boot_security/demo/controller/AdminRestController.java
+++ src/main/java/habsida/spring/boot_security/demo/controller/AdminRestController.java
@@ -1,5 +1,51 @@
 package habsida.spring.boot_security.demo.controller;
 
+import habsida.spring.boot_security.demo.model.User;
+import habsida.spring.boot_security.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin/users")
 public class AdminRestController {
+    private final UserService userService;
+
+    @Autowired
+    public AdminRestController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        userService.saveUser(
+                user.getUserFirstName(),
+                user.getUserFamilyName(),
+                user.getUsername(),
+                user.getPassword(),
+                user.getRoles().iterator().next().getName()
+        );
+        return user;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable("id") Long id) {
+        userService.removeUserById(id);
+    }
+
+    @PutMapping("/{id}")
+    public void updateUser(@PathVariable("id") Long id, @RequestBody User user) {
+        userService.updateUser(id,
+                user.getUserFirstName(),
+                user.getUserFamilyName()
+        );
+    }
+
 
 }
